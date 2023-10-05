4b3b6b76fad9cb6665722ca107f37890a612a91a
Anthony Holten <anthony.holten1@gmail.com>
2023-10-04 21:54:45 -0400

    add main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+class Program():
+    def __init__(self):
+        pass
+
+    def get_most_recent_observation(self):
+        return 0
+
+if __name__ == "__main__":
+    program = Program()
+    result = program.get_most_recent_observation()
+    print(result)
