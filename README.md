95ac7286871230f5f3b6c47abe6ad74a4122a490
Anthony Holten <anthony.holten1@gmail.com>
2023-10-05 21:03:41 -0400

    format

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # lanternfly-alerter
 
+[![Code style: black](https://img.shields.io/badge/code%20style-black-000000.svg)](https://github.com/psf/black)
+
 lanternfly-alerter is a WIP service that alerts users of new lanternfly observations using iNaturalist's API
--- main.py
+++ main.py
@@ -3,25 +3,26 @@ import requests
 base_url = "https://api.inaturalist.org/v1"
 taxon_id = 324726
 
-class Program():
+
+class Program:
     def __init__(self):
         pass
 
     def get_observation(self):
-        params = {
-            "per_page": 1,
-            "taxon_id": taxon_id
-        }
-        try: 
+        params = {"per_page": 1, "taxon_id": taxon_id}
+        try:
             response = requests.get(f"{base_url}/observations", params=params)
 
             if response.status_code != 200:
-                raise ValueError(f"API request failed with status code {response.status_code}: {response.text}")
-            
+                raise ValueError(
+                    f"API request failed with status code {response.status_code}: {response.text}"
+                )
+
             return response.json()
-        
+
         except ValueError as value_error:
             return value_error
+
 
 if __name__ == "__main__":
     program = Program()
